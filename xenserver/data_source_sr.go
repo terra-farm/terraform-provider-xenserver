51a3e6608b7967e4b923efcd5a0b01303786bfaf
Jon Cave <jon.cave@mwrinfosecurity.com>
2018-03-03 14:46:15 +0000

    Add SR data source
    Allows a Terraform configuration to dynamically determine the SR UUID to
    be used when creating new VDI resources. For example:
    
        data "xenserver_sr" "local-storage" {
          name_label = "Local storage"
        }
    
        resource "xenserver_vdi" "demo-vdi" {
          sr_uuid = "${data.xenserver_sr.local-storage.id}"
          name_label = "Demo"
          size = 536870912000
        }

---
+++ xenserver/data_source_sr.go
@@ -0,0 +1,52 @@
+package xenserver
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func dataSourceXenServerSR() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceXenServerSRRead,
+
+		Schema: map[string]*schema.Schema{
+			"name_label": &schema.Schema{
+				Type:        schema.TypeString,
+				Description: "The human readable name of the storage repository",
+				Required:    true,
+			},
+		},
+	}
+}
+
+func dataSourceXenServerSRRead(d *schema.ResourceData, meta interface{}) error {
+	c := meta.(*Connection)
+
+	nameLabel, nameLabelOk := d.GetOk("name_label")
+
+	if !nameLabelOk {
+		return fmt.Errorf("name_label must be provided")
+	}
+
+	if srs, err := c.client.SR.GetByNameLabel(c.session, nameLabel.(string)); err == nil {
+		found := false
+		for _, sr := range srs {
+			record, err := c.client.SR.GetRecord(c.session, sr)
+			if err != nil {
+				break
+			}
+
+			d.SetId(record.UUID)
+
+			found = true
+			break
+		}
+
+		if !found {
+			return fmt.Errorf("Matching SR not found")
+		}
+	}
+
+	return nil
+}
